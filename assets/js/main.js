271eaf344333a30641b68d9927692689636c6462
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-20 14:11:02 +0200

    initial commit: setup

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/* Consegna:
+ Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+*/
+
+// MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+// dichiaro un array e ci inserisco gli ogg. con i dati seguenti (nome, ruolo, foto)
+
+// MILESTONE 1: Stampare su console le informazioni di nome, ruolo e la stringa della foto
+   // ciclo for nell'array + console log(nome, ruolo, foto)
+
+// MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
+ // selezionare elemento della dom (queryselector), stampare ciò che avevo nel log precedente all'interno dell'elemento che ho selezionato
